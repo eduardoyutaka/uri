b955b7a4c97c2a14d75a8cc31b752e4c182771f7
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-04-03 18:01:32 -0300

    URI Beginner #2483

---
+++ beginner/2483.cpp
@@ -0,0 +1,18 @@
+// Feliz Nataaal!
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int I;
+  cin >> I;
+  cout << "Feliz nat";
+
+  while (I--) {
+    cout << 'a';
+  }
+
+  cout << "l!" << endl;
+
+  return 0;
+}
