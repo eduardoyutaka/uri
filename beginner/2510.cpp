ed1a547dad1725b4b5cefbd857c560ed8c2c32b8
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-04-05 15:33:20 -0300

    URI Beginner #2510

---
+++ beginner/2510.cpp
@@ -0,0 +1,16 @@
+// Batmain
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    string villain;
+    getline(cin, villain);
+    cout << 'Y' << endl;
+  }
+  return 0;
+}
