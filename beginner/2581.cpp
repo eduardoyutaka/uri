4cdbebda01fef0fa886442ee60800ed5a72ed509
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-04-05 19:36:47 -0300

    URI Beginner #2581

---
+++ beginner/2581.cpp
@@ -0,0 +1,15 @@
+// I am Toorg!
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int N;
+  string line;
+  cin >> N;
+  while (N--) {
+    getline(cin, line);
+    cout << "I am Toorg!\n";
+  }
+}
