439bad48544377dfa0f98ff589d5d8ba13ebee12
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-04-05 17:42:53 -0300

    URI Beginner #2544

---
+++ beginner/2544.cpp
@@ -0,0 +1,13 @@
+// Kage Bunshin no Jutsu
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int N;
+  while (cin >> N) {
+    cout << log2(N) << '\n';
+  }
+  return 0;
+}
