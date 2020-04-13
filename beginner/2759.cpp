c90a1d3200df6b102799b3e96af46c4c43feed30
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-04-13 02:57:32 -0300

    URI Beginner #2759

---
+++ beginner/2759.cpp
@@ -0,0 +1,13 @@
+// Entrada e Saída de Carácter
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char a, b, c;
+  cin >> a >> b >> c;
+  cout << "A = " << a << ", B = " << b << ", C = " << c << '\n';
+  cout << "A = " << b << ", B = " << c << ", C = " << a << '\n';
+  cout << "A = " << c << ", B = " << a << ", C = " << b << '\n';
+  return 0;
+}
